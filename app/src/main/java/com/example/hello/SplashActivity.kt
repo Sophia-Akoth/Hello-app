157e2b4a782d2f28e946bb16f9a3efa3a6accbc8
Sophia-Akoth <sophiadorcas@gmail.com>
2020-08-22 07:48:37 +0300

    Splash

--- app/src/main/java/com/example/hello/SplashActivity.kt
+++ app/src/main/java/com/example/hello/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hello
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,18 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(baseContext)
+        val accessToken = sharedPreferences.getString("ACCESS_TOKEN_KEY", "")
+
+        if (accessToken.isNullOrEmpty()){
+            val intent=Intent(baseContext,MainActivity::class.java)
+            startActivity(intent)
+        }
+        else{
+            val intent=Intent(baseContext,CoursesActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
